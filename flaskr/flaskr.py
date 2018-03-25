f657698c6f82810c908f1ac6f5a72f9606d4a2ca
Pihla Toivanen <toivanenpihla@gmail.com>
2018-03-25 18:25:32 +0300

    features

--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -1,6 +1,9 @@
 import os
 import sqlite3
 import json
+import operator
+import tempfile
+from tempfile import NamedTemporaryFile
 from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash
 
@@ -17,6 +20,7 @@ app.config.update(dict(
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+
 
 def connect_db():
     """Connects to the specific database."""
@@ -50,12 +54,71 @@ def initdb_command():
     init_db()
     print('Initialized the database.')
 
+def get_category_dict():
+    f = open("domain_subcategories.csv").read()
+    dicti = {}
+    for row in f.split("\n"):
+        if row == "":
+            continue
+        domain = row.split(",")[0]
+        cat = row.split(",")[1]
+        if domain not in dicti:
+            dicti[domain] = cat
+    return dicti
+
+def get_frequency_dict(domains):
+    dicti = {}
+    for item in domains:
+        for word in item.split(" "):
+            if not word.isspace() and word != "":
+                if word in dicti:
+                    dicti[word] = dicti[word] + 1
+                else:
+                    dicti[word] = 1
+    return dicti
+
+def get_class_dict(domain_dicti):
+    class_dict = get_category_dict()
+    class_freq_dict = {'not_found' : 0}
+    for key, value in domain_dicti.iteritems():
+        if key not in class_dict:
+            class_freq_dict['not_found'] += value
+            continue
+        category = class_dict[key]
+        if category in class_freq_dict:
+            class_freq_dict[category] = class_freq_dict[category] + value
+        else:
+            class_freq_dict[category] = value
+    return class_freq_dict
+
+def get_media_counts(media_id_list):
+    return_list = []
+    return_list.append(str(media_id_list.count(1)))
+    return_list.append(str(media_id_list.count(2)))
+    return_list.append(str(media_id_list.count(3)))
+    return_list.append(str(media_id_list.count(4)))
+    return_list.append(str(media_id_list.count(5)))
+    return_list.append(str(media_id_list.count(6)))
+    return ",".join(return_list)
+
+def get_domain_string(domainit):
+    returni = ""
+    for w in sorted(domainit, key=domainit.get, reverse=True):
+        returni += w + "," + str(domainit[w]) + "\n"
+    return returni
+
+def get_class_string(class_dict):
+    returni = ""
+    for w in sorted(class_dict, key=class_dict.get, reverse=True):
+        returni += w + "," + str(class_dict[w]) + "</br>"
+    return returni
 
 @app.route('/search', methods=['POST'])
 def search():
-    if request.form.get('search_word') == "":
-        return "Search word empty!"
+    #if request.form.get('search_word') == "":
+    #    return "Search word empty!"
     db = get_db()
+
     search_word = request.form.get('search_word')
     splitti = search_word.split(",")
     splitti_query = ""
@@ -65,10 +128,21 @@ def search():
     print(query)
     cur = db.execute('select * from posts where content like "%'+ splitti[0] + '%"' + splitti_query + 'order by id desc')
     entries = cur.fetchall()
+
     list = []
+    domain_list = []
+    media_id_list = []
+    links = []
     for row in entries:
-        list.append({"link": str(row['topic'].encode('utf-8'))})
-    return json.dumps({"posts": list})
+        domain_list.append(str(row['topic'].encode('utf-8')))
+        media_id_list.append(int(row['media_id']))
+        links.append(str(row['link'].encode('utf-8')))
+    domain_dicti = get_frequency_dict(domain_list)
+    class_dict = get_class_dict(domain_dicti)
+    print(class_dict)
+    domainit = domain_dicti
+    media_counts = get_media_counts(media_id_list)
+    return json.dumps({"classes": get_class_string(class_dict), "domains": get_domain_string(domainit), "media_counts": str(media_counts), "links": "\n".join(links)})
     return "Search executed"
 
 
